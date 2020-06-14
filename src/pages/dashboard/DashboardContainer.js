e19cb7e562e4a61a092eebff8b0be035c8891616
maxyee <eyaminkhan00@gmail.com>
2020-06-15 00:49:07 +0600

    Made the container for Dashboard page

---
+++ src/pages/dashboard/DashboardContainer.js
@@ -0,0 +1,13 @@
+import { getAllProducts } from '../../redux/action'
+import { connect } from 'react-redux'
+import Dashboard from './Dashboard'
+
+const mapStoreToProps = state => ({
+    products: state.product.products,
+    loading: state.product.loading
+})
+const mapDispatchToProps = dispatch => ({
+    getAllProducts: ()=>dispatch(getAllProducts())
+})
+
+export default connect(mapStoreToProps, mapDispatchToProps)(Dashboard)
